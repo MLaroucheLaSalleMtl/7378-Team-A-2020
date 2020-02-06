603aa74f379130131009c673d37514f117c9f259
Baiyang Chen <boyang96@yahoo.ca>
2020-02-06 00:38:30 -0500

    adding character
    character movement, first combat combo

---
+++ Sky_Of_Pisces/Assets/character_Folder/script/DragonControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DragonControl : MonoBehaviour
+{
+    public float lookRadios = 10f;
+    public Transform target;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadios)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadios);
+    }
+}
