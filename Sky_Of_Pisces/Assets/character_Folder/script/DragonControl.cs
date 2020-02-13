df52a042ebf3788139e5eb9476cedbdc485e1127
Baiyang Chen <boyang96@yahoo.ca>
2020-02-13 01:57:13 -0500

    Enemy wandering

--- Sky_Of_Pisces/Assets/character_Folder/script/DragonControl.cs
+++ Sky_Of_Pisces/Assets/character_Folder/script/DragonControl.cs
@@ -11,10 +11,16 @@ public class DragonControl : MonoBehaviour
     public float fov = 120f;
     public float viewDistance = 10;
     private NavMeshAgent agent;
+
+    public float wanderRadius = 7f;
+    private Vector3 wanderPoint;
+
+    public enum wanderpos { random, wayPoint};
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        wanderPoint = RandomWanderPos();
     }
 
     // Update is called once per frame
@@ -27,6 +33,7 @@ public class DragonControl : MonoBehaviour
         else
         {
             searchPlayer();
+            wander();
         }
     }
 
@@ -36,7 +43,14 @@ public class DragonControl : MonoBehaviour
         {
             if (Vector3.Distance(tpuc.transform.position, transform.position) < viewDistance)
             {
-                onAware();
+                RaycastHit hit;
+                if(Physics.Linecast(transform.position,tpuc.transform.position,out hit, -1))
+                {
+                    if (hit.transform.CompareTag("Player"))
+                    {
+                        onAware();
+                    }
+                }
             }
         }
     }
@@ -44,5 +58,25 @@ public class DragonControl : MonoBehaviour
     public void onAware()
     {
         isAware = true;
+    }
+
+    public void wander()
+    {
+        if (Vector3.Distance(transform.position, wanderPoint) < 2f)
+        {
+            wanderPoint = RandomWanderPos(); //have reached it, make a new wanderpoint
+        }
+        else
+        {
+            agent.SetDestination(wanderPoint); //have not reach it, move to the wanderpoint
+        }
+    }
+
+    public Vector3 RandomWanderPos()
+    {
+        Vector3 randomPos = (Random.insideUnitSphere * wanderRadius) + transform.position; //this sphere is made in the 0 orgin of the world, to make it in different we need to add the vector(transform)
+        NavMeshHit navhit;//to hold the property of resulting location
+        NavMesh.SamplePosition(randomPos, out navhit, wanderRadius, -1);//finds the closest point
+        return new Vector3(navhit.position.x, transform.position.y,navhit.position.z);  //returns the nearest x and z, y does not change
     }
 }
