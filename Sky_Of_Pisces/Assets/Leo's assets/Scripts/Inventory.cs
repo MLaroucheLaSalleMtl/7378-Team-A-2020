c490d33d48fa0ef7b12d9c462f1e449a3b481f4a
Chengyi Liao <lcy804585626@yahoo.ca>
2020-03-01 17:48:22 -0500

    New icon package and inventory script add in

---
+++ Sky_Of_Pisces/Assets/Leo's assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    private bool inventoryEnabled;
+    public GameObject inventory;//state inventory as a game object
+
+    private int allSlots;
+    private GameObject[] slot;//single instance for each slot variable, so it has to be an array
+
+    public GameObject slotCage;//state slotCage as a game object
+
+    void Start()
+    {
+        allSlots = 9 * 5;//number of slots
+        slot = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++)
+        {//get the game object's child transformation, reach slot as a game object type
+            slot[i] = slotCage.transform.GetChild(i).gameObject;
+        }
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {//make sure it switches between enabled and unenabled (open and close)
+            inventoryEnabled = !inventoryEnabled;
+        }
+        if (inventoryEnabled == true)
+        {//if inventory is not opened, this makes sure to open the inventory
+            inventory.SetActive(true);
+        }
+        else
+        {//if inventory is not closed, this makes sure to close the inventory
+            inventory.SetActive(false);
+        }
+    }
+}
