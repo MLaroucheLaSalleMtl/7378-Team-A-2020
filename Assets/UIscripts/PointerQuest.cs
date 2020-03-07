23b55f0fd17cfcfcb31df5ae4f7566fd8faab2dc
Haoqian gu <haoqiangu@gmail.com>
2020-03-07 17:00:32 -0500

    Add Boss scene and finish the main quest and collection quest

--- Assets/UIscripts/PointerQuest.cs
+++ Assets/UIscripts/PointerQuest.cs
@@ -7,23 +7,32 @@ public class PointerQuest : MonoBehaviour
 {
     public Image image;
     public Transform Eden;
+    //public Transform Ghost;
+
     public Text meter;
     public Vector3 offset;
     
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         //To set limition for the pointer on the screen
         float minX = image.GetPixelAdjustedRect().width / 2;
         float maxX = Screen.width - minX;
-        float minY = image.GetPixelAdjustedRect().width / 2;
-        float maxY = Screen.width - minY;
 
-        Vector2 position = Camera.main.WorldToScreenPoint(Eden.position+offset);
+        float minY = image.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.height - minY;
+
+        //Pointer for eden
+        Vector2 position = Camera.main.WorldToScreenPoint(Eden.position+offset);  
         position.x = Mathf.Clamp(position.x, minX, maxX);
         position.y = Mathf.Clamp(position.y, minY, maxY);
-        image.transform.position = position;
 
+        //Pointer for ghost
+        //Vector2 position1 = Camera.main.WorldToScreenPoint(Ghost.position + offset);
+        //position1.x = Mathf.Clamp(position1.x, minX, maxX);
+        //position1.y = Mathf.Clamp(position1.y, minY, maxY);
+
+        
         if (Vector3.Dot(Eden.position - transform.position, transform.forward) < 0)
         {
             //Target behind the player
@@ -36,7 +45,26 @@ public class PointerQuest : MonoBehaviour
                 position.x = minX;
             }
         }
-        meter.text = ((int)Vector3.Distance(Eden.position, transform.position)).ToString() + "M";
+        //if (Vector3.Dot(Ghost.position - transform.position, transform.forward) < 0)
+        //{
+        //    if (position1.x < Screen.width / 2)
+        //    {
+        //        position1.x = maxX;
+        //    }
+        //    else
+        //    {
+        //        position1.x = minX;
+        //    }
+        //}
+        
+        
 
+        //Info for eden
+        meter.text = ((int)Vector3.Distance(Eden.position, transform.position)).ToString() + "M";
+        image.transform.position = position;
+
+        //Info for ghost
+        //meter.text = ((int)Vector3.Distance(Ghost.position, transform.position)).ToString() + "M";
+        //image.transform.position = position;
     }
 }
