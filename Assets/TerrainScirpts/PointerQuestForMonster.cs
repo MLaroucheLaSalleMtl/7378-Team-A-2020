85259f42f7dcc45cee642ca834a605af7adee050
Haoqian gu <haoqiangu@gmail.com>
2020-03-08 21:49:52 -0400

    Added story scene and finalize the boss scene, modify the npc dialogue

---
+++ Assets/TerrainScirpts/PointerQuestForMonster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointerQuestForMonster : MonoBehaviour
+{
+    public Image image;
+    public Transform Eden;
+
+    public Text meter;
+    public Vector3 offset;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To set limition for the pointer on the screen
+        float minX = image.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+
+        float minY = image.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.height - minY;
+
+        //Pointer for eden
+        Vector2 position = Camera.main.WorldToScreenPoint(Eden.position + offset);
+        position.x = Mathf.Clamp(position.x, minX, maxX);
+        position.y = Mathf.Clamp(position.y, minY, maxY);
+
+
+
+        if (Vector3.Dot(Eden.position - transform.position, transform.forward) < 0)
+        {
+            //Target behind the player
+            if (position.x < Screen.width / 2)
+            {
+                position.x = maxX;
+
+            }
+            else
+            {
+                position.x = minX;
+
+            }
+
+
+        }
+
+        //Info for eden
+        meter.text = ((int)Vector3.Distance(Eden.position, transform.position)).ToString() + "M";
+        image.transform.position = position;
+
+    }
+}
---
+++ Assets/UIscripts/FadeIn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    public Image DeadBackground;
+    public Image DialogueBox;
+    public Image ContinueButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeadBackground.canvasRenderer.SetAlpha(0.0f);
+        DialogueBox.canvasRenderer.SetAlpha(0.0f);
+        ContinueButton.canvasRenderer.SetAlpha(0.0f);
+        fadeIn();
+    }
+
+    void fadeIn()
+    {
+        DeadBackground.CrossFadeAlpha(1, 2, false);
+        DialogueBox.CrossFadeAlpha(1, 2, false);
+        ContinueButton.CrossFadeAlpha(1, 2, false);
+    }
+}
