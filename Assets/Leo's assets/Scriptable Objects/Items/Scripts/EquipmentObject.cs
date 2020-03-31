be4fc1fabdab54702132b9cdde44d2155be62ea3
Chengyi Liao <lcy804585626@yahoo.ca>
2020-03-31 02:34:25 -0400

    New inventory system scripts 1
    Add a new series of inventory system script.
    Remove old, useless scripts and a scene.

---
+++ Assets/Leo's assets/Scriptable Objects/Items/Scripts/EquipmentObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventory System/Item/Equipment")]
+public class EquipmentObject : ItemObject
+{
+    public float atkBoost;
+    public float amrBoost;
+    public void Awake()
+    {
+        type = ItemType.Equipment;
+    }
+}
