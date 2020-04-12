6ef6acc8e4d0e8785c2ee9a516ab315d06e427d6
Chengyi Liao <lcy804585626@yahoo.ca>
2020-04-11 23:40:16 -0400

    Revert "Merge branch 'Leo's-2nd'"
    This reverts commit 55d6085b6366b61b96ba95f4cb0ed3bec2621101, reversing
    changes made to 5908047cf48a1b6a0c64e9b19fba21c07754928a.

--- Assets/Leo's assets/Scripts/DisplayInventory.cs
+++ Assets/Leo's assets/Scripts/DisplayInventory.cs
@@ -6,8 +6,7 @@ using TMPro;
 public class DisplayInventory : MonoBehaviour
 {
     public InventoryObject inventory;
-    public int X_START;
-    public int Y_START;
+
     public int X_SPACE_BETWEEN_ITEM;
     public int NUMBER_OF_COLUMN;
     public int Y_SPACE_BETWEEN_ITEM;
@@ -21,25 +20,7 @@ public class DisplayInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateDisplay();
-    }
-
-    public void UpdateDisplay()
-    {
-        for (int i = 0; i < inventory.Container.Count; i++)
-        {
-            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
-            {
-                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-            }
-            else
-            {
-                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-                itemsDisplayed.Add(inventory.Container[i], obj);
-            }
-        }
+        //UpdateDisplay();
     }
 
     public void CreateDisplay()
@@ -49,11 +30,10 @@ public class DisplayInventory : MonoBehaviour
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
-            itemsDisplayed.Add(inventory.Container[i], obj);
         }
     }
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), (Y_START + (-Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMN))), 0f);
+        return new Vector3(X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN), (-Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMN)), 0f);
     }
 }
