d14bf31342d1e1192bf20e0b37827ad4c8e80fd2
Baiyang Chen <boyang96@yahoo.ca>
2020-04-14 01:55:50 -0400

    finish boss movement

---
+++ Assets/character_Folder/script/BossState.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Boss_State
+{
+    sleep,
+    run,
+    attack,
+    skill,
+    death
+}
+
+public class BossState : MonoBehaviour
+{
+    private Transform player;
+    private Boss_State state = Boss_State.sleep;
+    private enemyHealth bossHp;
+    public int range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        bossHp = GetComponent<enemyHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        //Debug.Log(range);
+        if(state != Boss_State.death && bossHp.health < 100f)
+        {
+            if(distance <= 5)
+            {
+                state = Boss_State.attack;
+            }
+            else
+            {
+                state = Boss_State.run;
+            }
+
+        }
+        else if(bossHp.health <= 0)
+        {
+            state = Boss_State.death;
+        }
+    }
+
+    IEnumerator ramdonGenerate()
+    {
+        int attack_range = Random.Range(1, 5);
+        range = attack_range;
+        yield return new WaitForSeconds(5);
+        print(attack_range);
+    }
+    public Boss_State Boss_State
+    {
+        get { return state; }
+        set { Boss_State = value; }
+    }
+}
--- Assets/character_Folder/script/destroyEffect.cs
+++ Assets/character_Folder/script/destroyEffect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class destroyEffect : MonoBehaviour
 {
-    public float time = 2f;
+    public float time = 8f;
     // Start is called before the first frame update
     void Start()
     {
