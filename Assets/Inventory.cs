78a509482b2ad41264728eecce4f438fcad6686f
Haoqian gu <haoqiangu@gmail.com>
2020-04-15 12:52:48 -0400

    Modify the player, monster attack, hp, make it balance, help teammates

---
+++ Assets/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Update is called once per frame
+    public int lastPressed = 0;
+    public GameObject Inventroy;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            Inventroy.SetActive(true);
+             lastPressed++;
+            if (lastPressed > 1)
+            {
+                Inventroy.SetActive(false);
+                lastPressed = 0;
+            }
+        }
+
+    }
+}
